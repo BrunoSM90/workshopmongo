b24f08a846a2b6f5674676139ed3b7fd28702aff
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-15 19:01:40 -0300

    Tratamento de exceções para id not found

--- src/main/java/com/workshop/services/UserService.java
+++ src/main/java/com/workshop/services/UserService.java
@@ -1,12 +1,14 @@
 package com.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.entities.User;
 import com.workshop.repositories.UserRepository;
+import com.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 }
