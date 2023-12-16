39b586672eb0405617d9c2a56e14a92cbca627e7
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-15 22:55:28 -0300

    User post and delete

--- src/main/java/com/workshop/services/UserService.java
+++ src/main/java/com/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.entities.User;
+import com.workshop.entities.dto.UserDTO;
 import com.workshop.repositories.UserRepository;
 import com.workshop.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,22 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User FromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	public void delete(String id) {
+		User user = findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Object not found.");
+		}
+		
+		repo.delete(user);
 	}
 }
