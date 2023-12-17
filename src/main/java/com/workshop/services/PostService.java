189a989bbe4592cc019e80cbeafa41442e139a69
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-17 19:17:06 -0300

    post by id

--- src/main/java/com/workshop/services/PostService.java
+++ src/main/java/com/workshop/services/PostService.java
@@ -1,12 +1,14 @@
 package com.workshop.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshop.entities.Post;
 import com.workshop.repositories.PostRepository;
+import com.workshop.services.exception.ObjectNotFoundException;
 
 @Service
 public class PostService {
@@ -15,5 +17,10 @@ public class PostService {
 	
 	public List<Post> findAll() {
 		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
